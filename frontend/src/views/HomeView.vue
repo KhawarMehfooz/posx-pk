b673a9d5df9a7a0930def6e7cfdba8d632228b66
Khawar Mehfooz <khawarmehfooz565@gmail.com>
2025-11-21 15:09:55 +0500

    link frontend with backend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from "axios";
+import { ref } from "vue";
+import HelloWorld from '@/components/HelloWorld.vue';
+
+const serverResponse = ref("");
+
+async function testServer() {
+  try {
+    const res = await axios.get("http://localhost:3000/api/hello");
+    serverResponse.value = res.data; 
+    console.log(res.data);
+  } catch (err) {
+    console.error(err);
+    serverResponse.value = "Error contacting server";
+  }
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld msg="You did it!" />
+
+    <button @click="testServer">Test Server</button>
+
+    <!-- server response -->
+    <p v-if="serverResponse">{{ serverResponse }}</p>
   </main>
 </template>
