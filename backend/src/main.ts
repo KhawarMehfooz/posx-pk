f3ceb0e0b87d9964dc1ea030066f98c5eef3e2c5
M Ifraheem <mifrahim017@gmail.com>
2025-11-28 00:46:37 +0500

    nestJs project recreated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
-
-  app.setGlobalPrefix('api');
-
-  const port = process.env.port || 3000;
-  await app.listen(port);
-  console.log(`Backend server is running on http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
